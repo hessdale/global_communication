b41df0e097c7f6dd81a63237a3457daa372828c6
dale <hessdale96@gmail.com>
2023-04-22 10:39:45 -0600

    warming up with components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <toast-notification></toast-notification>
+    <delete-button></delete-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToastNotification from "@/components/ToastNotification.vue";
+import DeleteButton from "@/components/DeleteButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ToastNotification,
+    DeleteButton,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ToastNotification.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: undefined,
+    };
+  },
+  methods: {
+    toast_listen(toast) {
+      this.message = toast;
+    },
+  },
+  mounted() {
+    this.$root.$on(`toast_notify`, this.toast_listen);
+  },
+};
+</script>
+
+<style scoped>
+</style>
